ec9f5203a07bce14cdba50efead413c54c766eb1
brunogreg <78767805+brunogreg@users.noreply.github.com>
2024-02-24 21:35:59 -0300

    CALCULADORA SIMPLES ENDED

---
+++ calculadora_simples.py
@@ -0,0 +1,14 @@
+print("calculadora simples")
+num1= input(" Digite o primeiro número ")
+num2= input(" Digite o segundo número ")
+num1= float(num1)
+num2= float(num2)
+soma= num1 + num2
+soma1= num1 - num2
+soma2= num1 / num2
+soma3= num1 * num2 
+print("a soma dos dois números é", soma)
+print("a subtração dos dois numeros é", soma1)
+print("a divisao dos dois numeros é", soma2)
+print("a multiplicação dos dois numeros é", soma3)
+print("Deseja fazer outra conta? ")
